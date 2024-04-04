265a749721438a030f754502f8813e90b410835d
Chamin Im <ckals413@naver.com>
2024-04-04 21:42:08 +0900

    1회차

---
+++ app/src/main/java/com/example/floclone/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.example.floclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_home)
+    }
+}
