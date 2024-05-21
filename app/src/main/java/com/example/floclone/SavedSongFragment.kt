4046b2eadbe0a0b2286dee82254c9150bb87fe71
Chamin Im <ckals413@naver.com>
2024-05-22 03:00:09 +0900

    FEAT:어댑터 연결 및 레이아웃 매니저 설정

--- app/src/main/java/com/example/floclone/SavedSongFragment.kt
+++ app/src/main/java/com/example/floclone/SavedSongFragment.kt
@@ -25,12 +25,27 @@ class SavedSongFragment : Fragment(){
 
         //데이터 리스트 생성 더미 데이터
         savedSongDatas.apply {
+            add(SavedSong("Goodbye Yellow Breick Road","Elton John",R.drawable.img_album_exp7))
+            add(SavedSong("nightmares","easylife",R.drawable.img_album_exp8))
             add(SavedSong("내일의 우리","카더가든",R.drawable.img_album_exp3))
             add(SavedSong("Shake It Off","Tatlor Swift",R.drawable.img_album_exp))
             add(SavedSong("우리의 사랑은","찰리빈웍스",R.drawable.img_album_exp4))
+            add(SavedSong("TOMBOY","혁오",R.drawable.img_album_exp6 ))
             add(SavedSong("Ling Ling","검정치마",R.drawable.img_album_exp5))
-            add(SavedSong("Surf boy","김밥",R.drawable.img_album_exp6 ))
+            add(SavedSong("Surf boy","혁오",R.drawable.img_album_exp6 ))
+            add(SavedSong("Harmony","카더가든",R.drawable.img_album_exp3))
+            add(SavedSong("가죽자켓","혁오",R.drawable.img_album_exp6 ))
+            add(SavedSong("네번의 여름","카더가든",R.drawable.img_album_exp3))
+            add(SavedSong("지정석","혁오",R.drawable.img_album_exp6 ))
+            add(SavedSong("오래된 일","카더가든",R.drawable.img_album_exp3))
+            add(SavedSong("Paul","혁오",R.drawable.img_album_exp6 ))
+            add(SavedSong("내겐 그 아무도 없을 거야","카더가든",R.drawable.img_album_exp3))
+            add(SavedSong("Tokyo Inn","혁오",R.drawable.img_album_exp6 ))
         }
+
+        val savedSongRVAdapter = SavedSongRVAdapter(savedSongDatas)
+        binding.lockerSavedSongRecyclerView.adapter = savedSongRVAdapter
+        binding.lockerSavedSongRecyclerView.layoutManager = LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false)
 
 
 
